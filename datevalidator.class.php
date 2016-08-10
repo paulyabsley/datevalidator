83ebae9491bf6a6ace2ebaf2e6d9d5aa003eea64
Paul Yabsley <yabsleyp@gmail.com>
2016-08-10 20:46:26 +0100

    Add task comments to class

--- datevalidator.class.php
+++ datevalidator.class.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+Create a date validator in PHP that will check that a given date is a valid historical date. It will accept the date as a string, which is assumed to have come from user input. The user will have been told to use the format DD/MM/YYYY. Any non-valid dates or strings which do not match this format should be rejected. An example of it in use would be:
+ 
+$dateString = "03/12/1999";
+$result = DateValidator::validateHistoricalDate($dateString);
+if (!$result->isValid()) {
+    $message = $result->getMessage();
+}
+*/
+
 class DateValidator
 {
 
